4a22e9e61207cc6ec3d9dfe5f3342bd44d9c829e
pmikedakis <pmikedakis@gmail.com>
2018-06-07 16:48:52 +0300

    app test

--- server/services/index.js
+++ server/services/index.js
@@ -10,7 +10,7 @@ let msg = '';
 MongoClient.connect(url, (err, database) => {
     if(err) throw err;
 
-    db = database.db(databaseName);
+    db = database;//.db(databaseName);
     console.log("connected to database");
     msg = "initialized!";
 });
